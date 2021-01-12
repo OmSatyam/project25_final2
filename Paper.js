1ef47756ff982a6b1c60f97b556b945822c1c31d
OmSatyam <75114850+OmSatyam@users.noreply.github.com>
2021-01-12 17:05:05 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper{
+   constructor(x,y,width,height){
+     var options = {
+        'isStatic': false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
+     }  
+   this.body = Matter.Bodies.rectangle(x,y,width,height,options);
+   this.image = loadImage("paper.png")
+   this.width = width;
+   this.height = height;
+   World.add(world,this.body);
+   }
+
+display(){
+    var pos = this.body.position;
+    fill("pink");
+    push();
+    translate(pos.x,pos.y);
+    imageMode(CENTER);   
+    image(this.image,0,0,this.width,this.height);
+    pop();
+}
+}
+
